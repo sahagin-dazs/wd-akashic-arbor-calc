6e94892ef5ae3f9f0b4a966716642e658d6a74e0
sahagin-dazs <p.schiess@gmail.com>
2025-12-11 08:44:53 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const repoName = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split("/")[1]
+  : "";
+
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === "true" && repoName;
+const usingRootDomain =
+  repoName !== "" && repoName.toLowerCase().endsWith(".github.io");
+
 const basePath =
-  process.env.GITHUB_PAGES === "true" ? "/wd-akashic-arbor-calc/" : "/";
+  isGitHubPagesBuild && !usingRootDomain ? `/${repoName}/` : "/";
 
 export default defineConfig({
   plugins: [vue()],
